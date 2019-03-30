4757f07a79f4ebd558d7f2e237e8da330524e718
ThomasGodard <thomasgodard2@gmail.com>
2019-03-30 14:06:38 +0100

    formatage du code

--- server.js
+++ server.js
@@ -1,30 +1,38 @@
-const app = require('express')();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('./models/user');
-const User = mongoose.model('User');
+"use strict";
 
+const app = require("express")();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("./models/user");
+const User = mongoose.model("User");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, HEAD, OPTIONS, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "access-control-allow-origin, Accept, Content-Type");
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, HEAD, OPTIONS, POST, PUT, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "access-control-allow-origin, Accept, Content-Type"
+  );
+  next();
 });
 
-app.post('/login', async (req, res) => {
-    const {email, password} = req.body;
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-    const user = await User.findOne({'email': email})
-        .catch(err => console.log(err));
+  const user = await User.findOne({ email: email }).catch(err =>
+    console.log(err)
+  );
 
-    if (user)
-        if (password === user.hash_password) res.status(200).json({message: 'ok'});
-        else res.status(401).json({message: 'bad password'});
-    else res.status(401).json({message: 'email not exist'});
-
+  if (user)
+    if (password === user.hash_password)
+      res.status(200).json({ message: "ok" });
+    else res.status(401).json({ message: "bad password" });
+  else res.status(401).json({ message: "email not exist" });
 });
 
 // users/login
@@ -43,11 +51,11 @@ app.post('/login', async (req, res) => {
 
 // books/:id - DELETE
 
-mongoose.connect('mongodb://localhost/test', { keepAlive: 1, useNewUrlParser: true })
-    .then(() => {
-        app.listen(3030, () => {
-            console.log('⤷ http://localhost:3030');
-        });
-    })
-    .catch((err) => console.log(err));
-
+mongoose
+  .connect("mongodb://localhost/test", { keepAlive: 1, useNewUrlParser: true })
+  .then(() => {
+    app.listen(3030, () => {
+      console.log("⤷ http://localhost:3030");
+    });
+  })
+  .catch(err => console.log(err));
